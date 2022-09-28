0c4ed8cb08eecb6f8fbf1682e167cfec1f5c383f
Rohaan ansari <rohaanansari321@gmail.com>
2022-09-28 14:04:00 +0530

    changed inventory cols

--- src/Constants/InventoryData.js
+++ src/Constants/InventoryData.js
@@ -1,108 +1,118 @@
 export const InventoryData = [
-    {
-        "Product_id": "5940107005302237590",
-        "Name": "Apple",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 20
-    },
-    {
-        "Product_id": "5940107005302237591",
-        "Name": "Mango",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 120
-    },
-    {
-        "Product_id": "5940107005302237592",
-        "Name": "Banana",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 100
-    },
-    {
-        "Product_id": "5940107005302237593",
-        "Name": "Coffee",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 10
-    },
-    {
-        "Product_id": "5940107005302237594",
-        "Name": "Tea",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 200
-    },
-    {
-        "Product_id": "5940107005302237595",
-        "Name": "Apple",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 20
-    },
-    {
-        "Product_id": "5940107005302237596",
-        "Name": "Mango",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 120
-    },
-    {
-        "Product_id": "5940107005302237597",
-        "Name": "Banana",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 100
-    },
-    {
-        "Product_id": "5940107005302237598",
-        "Name": "Coffee",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 10
-    },
-    {
-        "Product_id": "5940107005302237599",
-        "Name": "Tea",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 200
-    },
-    {
-        "Product_id": "5940107005302237510",
-        "Name": "Apple",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 20
-    },
-    {
-        "Product_id": "5940107005302237521",
-        "Name": "Mango",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 120
-    },
-    {
-        "Product_id": "5940107005302237532",
-        "Name": "Banana",
-        "Description": "A short description of the product this can take upto 120 character of the space",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 100
-    },
-    {
-        "Product_id": "5940107005302237543",
-        "Name": "Coffee",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 10
-    },
-    {
-        "Product_id": "5940107005302237554",
-        "Name": "Tea",
-        "Description": "A short description of the product",
-        "Price_per_quantity": "100",
-        "Available_Quantity": 200
-    }
-
-]
+  {
+    Product_id: "5940107005302237590",
+    Name: "Apple",
+    Category: "Fruit",
+    SKUCode: 909865,
+    Quantity: 20,
+    cancellable: "true",
+    returnable: "false",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 20,
+  },
+  {
+    Product_id: "5940107005302237591",
+    Name: "Mango",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 120,
+  },
+  {
+    Product_id: "5940107005302237592",
+    Name: "Banana",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 100,
+  },
+  {
+    Product_id: "5940107005302237593",
+    Name: "Coffee",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 10,
+  },
+  {
+    Product_id: "5940107005302237594",
+    Name: "Tea",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 200,
+  },
+  {
+    Product_id: "5940107005302237595",
+    Name: "Apple",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 20,
+  },
+  {
+    Product_id: "5940107005302237596",
+    Name: "Mango",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 120,
+  },
+  {
+    Product_id: "5940107005302237597",
+    Name: "Banana",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 100,
+  },
+  {
+    Product_id: "5940107005302237598",
+    Name: "Coffee",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 10,
+  },
+  {
+    Product_id: "5940107005302237599",
+    Name: "Tea",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 200,
+  },
+  {
+    Product_id: "5940107005302237510",
+    Name: "Apple",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 20,
+  },
+  {
+    Product_id: "5940107005302237521",
+    Name: "Mango",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 120,
+  },
+  {
+    Product_id: "5940107005302237532",
+    Name: "Banana",
+    Description:
+      "A short description of the product this can take upto 120 character of the space",
+    Price_per_quantity: "100",
+    Available_Quantity: 100,
+  },
+  {
+    Product_id: "5940107005302237543",
+    Name: "Coffee",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 10,
+  },
+  {
+    Product_id: "5940107005302237554",
+    Name: "Tea",
+    Description: "A short description of the product",
+    Price_per_quantity: "100",
+    Available_Quantity: 200,
+  },
+];
