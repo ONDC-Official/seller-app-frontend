2560a9b1366f561228f370a3a2c32da2685266b2
Mayur Jadhav <mj.jadhav13@gmail.com>
2023-03-13 15:44:01 +0530

    Integrate bulk upload API

--- src/Components/Application/Product/BulkUpload.jsx
+++ src/Components/Application/Product/BulkUpload.jsx
@@ -3,9 +3,30 @@ import MyButton from "../../Shared/Button";
 import { useNavigate } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Navbar from "../../Shared/Navbar";
+import { getCall, postCall } from "../../../Api/axios.js";
+import { Link, Button } from "@mui/material";
+import cogoToast from "cogo-toast";
 
 const BulkUpload = () => {
   const navigate = useNavigate();
+  const [selectedFile, setSelectedFile] = React.useState(null);
+
+  const uploadSelectedFile = () => {
+    if (selectedFile) {
+      const formData = new FormData();
+      formData.append("xlsx", selectedFile);
+      postCall("api/v1/products/upload/bulk", formData)
+        .then(resp => {
+          cogoToast.success("Product added successfully!");
+        }).catch(error => {
+          console.log(error);
+          cogoToast.error(error.response.data.error);
+        });
+    } else {
+      alert("Please select the file to upload");
+    }
+  }
+
   return (
     <div>
       <Navbar />
@@ -25,17 +46,22 @@ const BulkUpload = () => {
           </label>
           <div className="mt-6 flex flex-col">
             <label className="ml-2 md:mb-4 md:mt-3 mt-2 font text-xm">
-              Choose an excel file.
+              Please select an excel file. To download sample template, click <Link href={`${process.env.REACT_APP_BASE_URL}/api/v1/products/upload/bulk/template`} target="_blank" style={{}}>here</Link>
             </label>
             <input
               className="ml-2"
               id="contained-button-file"
               style={{ opacity: "none" }}
-              accept=".csv"
+              accept=".xlsx"
               type="file"
-              onChange={(e) => console.log(e.target.files[0])}
+              onChange={(e) => setSelectedFile(e.target.files[0])}
               //   key={item?.id}
             />
+          </div>
+          <div className="mt-6 flex flex-col-1">
+            <Button variant="contained" color="primary" onClick={uploadSelectedFile}>
+              Upload
+            </Button>
           </div>
         </div>
       </div>
