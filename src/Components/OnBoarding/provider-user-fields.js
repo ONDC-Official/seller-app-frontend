8ec1ec5ba639e1cafcefbf952eaca12e90c8b8ad
Vaibhav Raut <vaibhav@wemotiveforge.com>
2023-03-24 16:58:46 +0530

    fix: form validations added on invite provider form

---
+++ src/Components/OnBoarding/provider-user-fields.js
@@ -0,0 +1,25 @@
+export default [
+    {
+      id: "email",
+      title: "Email",
+      placeholder: "Enter your email address",
+      type: "input",
+      email: true,
+      required: true,
+    },
+    {
+      id: "mobile",
+      title: "Mobile Number",
+      placeholder: "Enter your mobile number",
+      type: "input",
+      mobile: true,
+      required: true,
+    },
+    {
+      id: "name",
+      title: "Name",
+      placeholder: "Enter provider first name",
+      type: "input",
+      required: true,
+    },
+];
