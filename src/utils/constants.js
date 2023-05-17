d0636620d9dea6d97b62ccc44f620fd8e4a1efa2
vishal-wemotive <vishal@wemotiveforge.com>
2023-05-17 11:53:32 +0530

    fix: Importer FSSAI License No field should have limited length as (14) (Accept only numeric value), Brand Owner FSSAI License No field should have limited length as (14) (Accept only numeric value).

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const MAX_STRING_LENGTH = 255;
 export const MAX_STRING_LENGTH_50 = 50;
+export const MAX_STRING_LENGTH_14 = 14;
 
 export const PRODUCT_CATEGORY = {
     "grocery": "Grocery",
