1d8b885b3ab4f11d266dd975323511474563fe94
vishal-wemotive <vishal@wemotiveforge.com>
2023-05-17 12:19:55 +0530

    fix: Product code should have limited digit (13) to be entered. Only allow numeric inputs.

--- src/Components/Application/Product/AddProduct.jsx
+++ src/Components/Application/Product/AddProduct.jsx
@@ -11,7 +11,7 @@ import { getCall, postCall, putCall } from "../../../Api/axios";
 import useForm from '../../../hooks/useForm'
 import { containsOnlyNumbers } from '../../../utils/formatting/string'
 import BackNavigationButton from "../../Shared/BackNavigationButton";
-import { PRODUCT_SUBCATEGORY, FIELD_NOT_ALLOWED_BASED_ON_PROTOCOL_KEY, MAX_STRING_LENGTH, MAX_STRING_LENGTH_50, MAX_STRING_LENGTH_14, MAX_STRING_LENGTH_3, MAX_STRING_LENGTH_6, MAX_STRING_LENGTH_10 } from "../../../utils/constants";
+import { PRODUCT_SUBCATEGORY, FIELD_NOT_ALLOWED_BASED_ON_PROTOCOL_KEY, MAX_STRING_LENGTH, MAX_STRING_LENGTH_50, MAX_STRING_LENGTH_14, MAX_STRING_LENGTH_3, MAX_STRING_LENGTH_6, MAX_STRING_LENGTH_10, MAX_STRING_LENGTH_13 } from "../../../utils/constants";
 import {isAmountValid, isNumberOnly} from '../../../utils/validations';
 import productFields from './product-fields'
 
@@ -211,7 +211,7 @@ export default function AddProduct() {
 
   const validate = () => {
     let formErrors = {}
-    formErrors.productCode = formValues?.productCode?.trim() === '' ? 'Product code is not allowed to be empty' : formValues?.productCode?.length > MAX_STRING_LENGTH ? `Cannot be more than ${MAX_STRING_LENGTH} characters` : '';
+    formErrors.productCode = formValues?.productCode?.trim() === '' ? 'Product code is not allowed to be empty' : !isNumberOnly(formValues?.productCode) ? 'Please enter only digit' : formValues?.productCode?.length > MAX_STRING_LENGTH_13 ? `Cannot be more than ${MAX_STRING_LENGTH_13} characters` : '';
     formErrors.productName = formValues?.productName?.trim() === '' ? 'Product name is not allowed to be empty' : formValues?.productName?.length > MAX_STRING_LENGTH ? `Cannot be more than ${MAX_STRING_LENGTH} characters` : '';
     formErrors.MRP = !formValues?.MRP ? 'Please enter a valid number' : !isAmountValid(formValues?.MRP)?'Please enter only digit':''
     formErrors.retailPrice = !formValues?.retailPrice ? 'Please enter a valid number' : !isAmountValid(formValues?.retailPrice)?'Please enter only digit':''
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,8 @@ export const MAX_STRING_LENGTH_14 = 14;
 export const MAX_STRING_LENGTH_10 = 10;
 export const MAX_STRING_LENGTH_3 = 3;
 export const MAX_STRING_LENGTH_6 = 6;
+export const MAX_STRING_LENGTH_8 = 8;
+export const MAX_STRING_LENGTH_13 = 13;
 
 export const PRODUCT_CATEGORY = {
     "grocery": "Grocery",
