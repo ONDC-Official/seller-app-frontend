2d67bc57909c92371f43e9daafe92e982f774e86
Rohaan ansari <rohaanansari321@gmail.com>
2023-06-16 16:59:01 +0530

    removed password field from invite provider screen

--- src/Components/OnBoarding/provider-user-fields.js
+++ src/Components/OnBoarding/provider-user-fields.js
@@ -1,34 +1,26 @@
 export default [
-    {
-      id: "email",
-      title: "Email",
-      placeholder: "Enter Provider Email Address",
-      type: "input",
-      email: true,
-      required: true,
-    },
-    {
-      id: "mobile",
-      title: "Mobile Number",
-      placeholder: "Enter Provider Mobile Number",
-      type: "input",
-      mobile: true,
-      maxLength: 10,
-      required: true,
-    },
-    {
-      id: "name",
-      title: "Name",
-      placeholder: "Enter Provider Name",
-      type: "input",
-      required: true,
-    },
-    {
-      id: "password",
-      title: "Password",
-      placeholder: "Enter Password",
-      type: "input",
-      required: true,
-      password: true
-    },
+  {
+    id: "email",
+    title: "Email",
+    placeholder: "Enter Provider Email Address",
+    type: "input",
+    email: true,
+    required: true,
+  },
+  {
+    id: "mobile",
+    title: "Mobile Number",
+    placeholder: "Enter Provider Mobile Number",
+    type: "input",
+    mobile: true,
+    maxLength: 10,
+    required: true,
+  },
+  {
+    id: "name",
+    title: "Name",
+    placeholder: "Enter Provider Name",
+    type: "input",
+    required: true,
+  },
 ];
