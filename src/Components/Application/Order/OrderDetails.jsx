2e6d5670c0e3dda19ab007e0639e28030fc8e2cd
Rohaan ansari <rohaanansari321@gmail.com>
2023-06-17 22:10:07 +0530

    fixed product additional indo border bottom

--- src/Components/Application/Order/OrderDetails.jsx
+++ src/Components/Application/Order/OrderDetails.jsx
@@ -486,7 +486,7 @@ const OrderItemsSummaryCard = (props) => {
                 let product = order_item?.details;
                 return (
                   <ExpandableTableRow
-                    sx={{ "& > *": { borderBottom: "unset" } }}
+                    sx={{ "& > *": { borderBottom: "1px solid #e0e0e0" } }}
                     key={order_item.order_id}
                     expandComponent={
                       <TableCell colSpan="7">
