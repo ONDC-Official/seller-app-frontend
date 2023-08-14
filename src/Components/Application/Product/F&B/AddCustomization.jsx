ab8a51d40dccf491ffca59a13cfff440239c8c42
Rohaan ansari <rohaanansari321@gmail.com>
2023-08-14 13:46:44 +0530

    remove: fields.js and moved fields to addCustomization.jsx

--- src/Components/Application/Product/F&B/AddCustomization.jsx
+++ src/Components/Application/Product/F&B/AddCustomization.jsx
@@ -1,11 +1,84 @@
 import React, { useState } from "react";
 import { Button, Modal } from "@mui/material";
 import RenderInput from "../../../../utils/RenderInput";
-import { customizationFields } from "./fields";
 
 const containerClasses = "flex items-center";
 const inputClasses = "w-80 h-full px-2.5 py-3.5 text-[#606161] bg-transparent !border-black flex";
 const labelClasses = "w-40 my-4 text-sm py-2 ml-1 font-medium text-left text-[#606161] inline-block";
+
+export const customizationFields = [
+  {
+    id: "name",
+    title: "Name",
+    placeholder: "Enter Customization Name",
+    type: "input",
+  },
+  {
+    id: "price",
+    title: "Price",
+    placeholder: "Enter Customization Price",
+    type: "number",
+  },
+  {
+    id: "UOM",
+    title: "UOM",
+    placeholder: "Select Unit Of Measurement",
+    options: [
+      { key: "unit", value: "unit" },
+      { key: "dozen", value: "dozen" },
+      { key: "gram", value: "gram" },
+      { key: "kilogram", value: "kilogram" },
+      { key: "tonne", value: "tonne" },
+      { key: "litre", value: "litre" },
+      { key: "millilitre", value: "millilitre" },
+    ],
+    type: "select",
+    inputStyles: {
+      width: 320,
+    },
+    disableClearable: true,
+  },
+  {
+    id: "UOMValue",
+    title: "UOMValue",
+    placeholder: "Enter UOM Value",
+    type: "input",
+  },
+  {
+    id: "available",
+    title: "Available Quantity",
+    placeholder: "Enter Available Quantity",
+    type: "number",
+  },
+  {
+    id: "maximum",
+    title: "Maximum Quantity",
+    placeholder: "Enter Maximum Quantity",
+    type: "number",
+  },
+  {
+    id: "vegNonVeg",
+    title: "Veg/Non-Veg/Egg",
+    placeholder: "Select Food Category",
+    type: "select",
+    options: [
+      { key: "Veg", value: "VEG" },
+      { key: "Non Veg", value: "NONVEG" },
+      { key: "Egg", value: "EGG" },
+    ],
+    disableClearable: true,
+    inputStyles: {
+      width: 320,
+    },
+  },
+  {
+    id: "parent",
+    title: "Parent",
+    placeholder: "Enter Customization Name",
+    type: "input",
+    isDisabled: true,
+  },
+];
 
 const AddCustomization = (props) => {
   const {
